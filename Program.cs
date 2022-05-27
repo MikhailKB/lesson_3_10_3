8268140bde2cea23452e3dbdd07a8450d4f598a4
Михаил Козин <Vasago85@mail.ru>
2022-05-27 22:17:02 +0300

    lesson_3_10_3

--- Program.cs
+++ Program.cs
@@ -8,22 +8,22 @@ namespace lesson_3_10_3
         static void Main(string[] args)  // точка входа
         {
 
-            {
-                Write("\nЗдравствуйте! Введите число и нажмите \"Enter\": ");
+            
+                Write($"Здравствуйте! Введите число и нажмите \"Enter\": ");
+                int count = 0;
                 int whole = int.Parse(ReadLine());
                 bool n = true;
                 int i = 2;
                 while (i <= Math.Sqrt(whole))
+                if (whole % i == 0) // если whole делится без остатка на i - возвращаем false (число не простое)
                 {
-                    if (whole % i == 0) // если whole делится без остатка на i - возвращаем false (число не простое)
-                    {
-                        n = false;
-                        break;
-                    }
-                    else
-                    {
-                        i++;
-                    }
+                    n = false;
+                        count++; 
+                    break;
+                }
+                else
+                {
+                    i++;
                 }
                 if (n == true)
                 {
@@ -34,7 +34,7 @@ namespace lesson_3_10_3
                    WriteLine("Число непростое");
                 }
                 ReadKey();
-            }
+            
         }
     }
 }
